1d69f8bcd8a0d05dd84817da90aee8a91304bc89
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-03-06 14:21:41 -0300

    feat: test function in routes.ts

---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+import {
+    FastifyInstance,
+    FastifyPluginOptions,
+    FastifyRequest,
+    FastifyReply
+} from "fastify"
+
+export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions){
+
+    fastify.get("/teste", (request: FastifyRequest, reply: FastifyReply) => {
+        console.log("ROTA CHAMADA")
+
+        reply.send({ ok: true})
+    })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import Fastify from "fastify";
-import cors from "cors";
+import cors from "@fastify/cors";
 import dotenv from "dotenv";
+import { routes } from "./routes";
 
 const app = Fastify({ logger: true})
 dotenv.config();
@@ -10,7 +11,15 @@ app.setErrorHandler((error, request, reply) => {
 })
 
 const start = async () => {
+    app.register(cors);
+    app.register(routes);
 
+    try {
+        await app.listen({ port: 3333, host: "0.0.0.0"})
+        console.log(`Servidor rodando`)
+    } catch (error) {
+        console.log(err);
+    }
 }
 
 start();
