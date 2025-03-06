865d364486c6d8a2072b29f5e469cb4409ef3ece
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-03-06 14:51:33 -0300

    feat: create controller CreateNutrition

---
+++ src/controllers/CreateNutritionController.ts
@@ -0,0 +1,9 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+
+class CreateNutritionController{
+    async handle(request: FastifyRequest, reply: FastifyReply){
+        console.log("ROTA FOI CHAMADA!!!")
+
+        reply.send({ message: "rota foi chamada"})
+    }
+}
